0efe1fb7ed24fa0803a8976d0aeeec45ff8019c2
Eugene Matveenko work <matvej4ik@gmail.com>
2018-04-12 16:28:54 +0300

    Started Task 3 in HW9 and introduced the code of BattleShip

--- src/HW9/hw9.js
+++ src/HW9/hw9.js
@@ -1,6 +1,31 @@
+//Task 1
 function randomInteger(min, max) {
     let rand = Math.random() * (max - min) + min;
     return Math.round ( rand );
 }
 
 console.log ( randomInteger (1, 100) );
+
+
+//Task 2
+let user ={};
+user.name = 'Вася';
+user.surname = 'Петров'
+user.name = 'Сергей';
+delete user.name;
+
+console.log (user);
+
+
+//Task 3
+function isEmpty(obj) {
+    let property = 45654;    
+}
+
+console.log  (isEmpty(obj.property));
+// if (isEmpty(obj) == 0) {
+//     console.log (isEmpty);
+// }
+// else { 
+//     return false;
+// }
